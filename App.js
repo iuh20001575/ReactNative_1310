81c18768b4513b37277134c026c7b6365986fc11
Hà Anh Thảo <thaoanhhaa1@gmail.com>
2023-10-16 16:49:07 +0700

    Add navigation

--- App.js
+++ App.js
@@ -1,6 +1,25 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Screen01 from './src/screens/screen01';
 import Screen02 from './src/screens/screen02';
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-    return <Screen01 />;
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='screen_01'>
+                <Stack.Screen
+                    name='screen_01'
+                    component={Screen01}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name='screen_02'
+                    component={Screen02}
+                    options={{ headerShown: false }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
